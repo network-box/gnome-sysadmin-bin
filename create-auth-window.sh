94093fd1a76af53c5f3ad576f3f1d4f4143b23e9
Ross Golder <rossg@src.gnome.org>
2005-08-21 04:02:24 +0000

    Initial revision
    svn path=/trunk/; revision=2

---
+++ create-auth-window.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/home/admin/bin/create-auth bugsro bugzilla ftpadmin gnomeweb halloween webusers 
---
+++ update-pserver-pword.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+LIVEFILE=/cvs/gnome/CVSROOT/passwd
+TMPFILE=/tmp/cvspword.$$
+PATCHFILE=/tmp/cvspword.diff.$$
+
+/home/admin/bin/update-pserver-pword >$TMPFILE
+
+# Check for differences
+diff $LIVEFILE $TMPFILE > $PATCHFILE
+patchlength=`cat $PATCHFILE | wc -l`
+
+# If no differences, our work is done
+if [ $patchlength -eq 0 ]; then
+	#echo "No changes";
+	exit 0;
+fi
+
+# If differences are extreme, dump the TMPFILE with an error message
+if [ $patchlength -gt 50 ]; then
+	echo "Generated password file radically different to existing one."
+	exit 1;
+fi
+
+# Basic checks complete, instate new file
+cat $TMPFILE > $LIVEFILE
+
+# Report changes to sysadmin
+cat $PATCHFILE
+
+# Tidyup
+rm -f $TMPFILE
