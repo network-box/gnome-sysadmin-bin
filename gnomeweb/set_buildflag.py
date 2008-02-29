a6affdc111b96f78b4d4b8387bac5c6a74d83b24
Olav Vitters <ovitters@src.gnome.org>
2008-02-29 21:31:48 +0000

    Add support for branches (sorta hacky).
    	* gnomeweb/set_buildflag.py: Add support for branches (sorta hacky).
    
    
    svn path=/trunk/; revision=58

--- gnomeweb/set_buildflag.py
+++ gnomeweb/set_buildflag.py
@@ -6,27 +6,35 @@
 
 import sys
 import os
-import getopt
+import os.path
+from optparse import OptionParser
 
 timestamp_dir = "/usr/local/www/gnomeweb/timestamps"
 
 def usage():
-	print "usage: " + sys.argv[0] + " [-h] [-m <module>]"
-	print "  -m    Set the build flag for <modulename>"
+    print "usage: " + sys.argv[0] + " [-h] [-m <module>]"
+    print "  -m    Set the build flag for <modulename>"
 
-try:                                
-	opts, args = getopt.getopt(sys.argv[1:], "hm:", ["help", "module="])
-except getopt.GetoptError:
-        usage()
-        sys.exit()
-for opt, arg in opts:
-	if opt in ("-m", "--module"):
-		module = arg
-if(module == ""):
-	usage()
-	sys.exit()
+parser = OptionParser()
+parser.add_option("-m", "--module", dest="module",
+                          help="Module to trigger an update for")
+parser.add_option("-b", "--branch", dest="branch",
+                          help="Branch to update")
 
-build_flag = timestamp_dir + "/" + module + ".buildflag"
+parser.set_defaults(kill=False, remove_seen=False, delete=False)
+
+(opts, args) = parser.parse_args()
+
+if not opts.module:
+    parser.print_usage()
+    sys.exit(1)
+
+if opts.branch:
+    module = '%s!%s' % (opts.module, opts.branch)
+else:
+    module = opts.module
+
+build_flag = os.path.join(timestamp_dir, module + ".buildflag")
 
 # Create the flag file (write the first line of the e-mail, which
 # I suppose can help identify the last committer)
