9f9aecf4e7467195c97bbf2d8c26ff72eae71bb7
Olav Vitters <ovitters@src.gnome.org>
2008-11-03 22:57:24 +0000

    Set mime types for Python programs (enables syntax highlighting).
    svn path=/trunk/; revision=85

--- copy-db.py
+++ copy-db.py
@@ -8,23 +8,23 @@ uidbs = {} # Databases not to be backed up, read from db-list
 
 # Get available DBs list
 for db in os.popen ('mysqlshow').readlines ()[3:-1]:
-	dbs.append (db.replace ('|', ' ').strip ())
+    dbs.append (db.replace ('|', ' ').strip ())
 
 # Get not-to-be-backed-up list
 list = open ('/root/bin/db-list')
 for line in list.readlines ():
-	if not line.startswith ('#'):
-		dbname, who, when = line.strip ().split ()
-		uidbs[dbname] = (who, when)
+    if not line.startswith ('#'):
+        dbname, who, when = line.strip ().split ()
+        uidbs[dbname] = (who, when)
 
 # Spit warnings and remove not-to-be-backed-up databases from the list
 for i in uidbs:
-	if i not in dbs:
-		sys.stderr.write ('WARNING: redundant entry for database %s in db-list\n\n' % i)
-	else:
-		sys.stderr.write ('WARNING: database %s not being backed up (request by %s on %s)\n\n' % (i, uidbs[i][0], uidbs[i][1]))
-		dbs.remove (i)
+    if i not in dbs:
+        sys.stderr.write ('WARNING: redundant entry for database %s in db-list\n\n' % i)
+    else:
+        sys.stdout.write ('WARNING: database %s not being backed up (request by %s on %s)\n\n' % (i, uidbs[i][0], uidbs[i][1]))
+        dbs.remove (i)
 
 # Backup!
 for db in dbs:
-	os.system ('mysqlhotcopy --allowold %s /var/lib/mysql-backup' % db)
+    os.spawnlp(os.P_WAIT, 'mysqlhotcopy', 'mysqlhotcopy', '--quiet', '--allowold', db, '/var/lib/mysql-backup')
