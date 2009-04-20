ec57670b7fabe084520b56e192d38f1364011b6a
Owen W. Taylor <otaylor@fishsoup.net>
2009-04-20 11:44:39 -0400

    Give gitadmin-bin access to fixed.gnome.org
    Provide a login to a x86_64 box with devel environment for gitadmin-bin
    to work on migration code.

--- create-auth-fixed.sh
+++ create-auth-fixed.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-/home/admin/bin/create-auth buildmaster buildslave
+# gitadmin here temporarily to give people a login to a x86_64 box with
+# devel environment
+/home/admin/bin/create-auth buildmaster buildslave gitadmin
