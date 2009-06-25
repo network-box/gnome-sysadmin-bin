74f5f5b2b7450ee89504c2534e0de2782e655b9c
Olav Vitters <olav@bkor.dhs.org>
2009-06-25 20:33:58 +0200

    Remove obsolete scripts
    Not needed thanks to puppet.

--- create-auth-fixed.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# gitadmin here temporarily to give people a login to a x86_64 box with
-# devel environment
-/home/admin/bin/create-auth buildmaster buildslave gitadmin
--- create-auth-window.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/home/admin/bin/create-auth accounts ftpadmin gnomeweb webusers
