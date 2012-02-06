1e07aff38cbc0a70bef5358439218bdfe88a2b9f
Andrea Veri <av@src.gnome.org>
2012-02-06 15:14:19 +0100

    Use xargs instead of -exec

--- clean_nagios_logs.sh
+++ clean_nagios_logs.sh
@@ -3,5 +3,4 @@
 LOGS_DIR=/var/log/nagios3/archives
 
 # Cleanup older than 5 days nagios logs
-find $LOGS_DIR/* -mtime +5 -exec rm {} \;
-
+find $LOGS_DIR/* -type f -mtime +5 -print0 | xargs -0 -r rm -f
