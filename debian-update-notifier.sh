f583349ceb20b51d83653ab349a0252f623870e6
Mathieu Bridon <bochecha@fedoraproject.org>
2014-01-23 17:30:56 +0800

    Keep only what we need

--- debian-update-notifier.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-aptitude update > /dev/null
-
-t=`aptitude -F %p search ~U`
-if [[ "$t" =~ '[[:alpha:]]' ]]; then
-    echo "Updates available for the following package(s) are available:"
-    echo $t
-fi
